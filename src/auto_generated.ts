56b33105e27d2d5d1b1b5c4f8e894860ae7b9e6f
greinisch <greinisch@youwol.com>
2021-10-06 15:38:03 +0200

    :bookmark: 0.0.11

--- src/auto_generated.ts
+++ src/auto_generated.ts
@@ -1,6 +1,6 @@
 export let AUTO_GENERATED = {
     name: "@youwol/flux-three",
     namespace: "youwol",
-    version: "0.0.10",
+    version: "0.0.11",
     description: "Collection of modules for Flux to render 3D scenes. Wrapper around three.js."
 }
